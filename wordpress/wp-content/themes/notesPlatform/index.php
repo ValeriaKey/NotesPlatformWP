f8b33751b6556512a68ef221c9a2be1e05c63226
ValeriaNovak <valeria@amanduscommunication.se>
2023-04-04 15:09:44 +0300

    base/create own theme

---
+++ wordpress/wp-content/themes/notesPlatform/index.php
@@ -0,0 +1,4 @@
+<h1>Welcome to small project!</h1>
+<p>Register now and try use this tool for your time management!</p>
+<a href="#" class="btn btn-login">Login in</a>
+<a href="#" class="btn btn-register">Register</a>
